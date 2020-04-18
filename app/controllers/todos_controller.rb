d2cc6a1b507eae1fda2161e21a505e2d75e77270
laashiv <vishnav2502@gmail.com>
2020-04-18 13:20:43 +0530

    Update database config

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,6 @@
 class TodosController < ApplicationController
   def index
-    render plain: "Hello, this is /todos!"
+    render plain: "Hello to Rails! #{DateTime.now.to_s(:short)}"
   end
 end
 
