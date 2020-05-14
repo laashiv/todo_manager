9842d1c7eb8ea38efd6285a74d872b5b6a24abb5
laashiv <vishnav2502@gmail.com>
2020-05-14 20:04:02 +0530

    Ensure no nil name, email or password among users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,17 @@ class UsersController < ApplicationController
   end
 
   def create
-    User.create!(
+    user = User.new(
       first_name: params[:first_name],
       last_name: params[:last_name],
       email: params[:email],
       password: params[:password]
     )
-    redirect_to "/"
+    if user.save
+      redirect_to "/"
+    else
+      flash[:error] = user.errors.full_messages.join(", ")
+      redirect_to new_user_path
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :todos
+
+  validates :first_name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
 end
